fc88424a916b482b0ba98aec5136f3160bb2415b
AzureVauban <haloringman@outlook.com>
2022-11-17 18:21:11 -0600

    initialization commit

---
+++ NodeUtility.h
@@ -0,0 +1,53 @@
+//
+// Created by halor on 11/17/2022.
+//
+
+#pragma once
+
+#include <iostream>
+#include <utility>
+#include <vector>
+
+namespace NodeUtility {
+
+    struct Base {
+        std::string ingredient;
+        int amountonhand;
+
+        Base(std::string ingredient = "", int amountonhand = 0) {
+            this->ingredient = ingredient;
+            this->amountonhand = amountonhand;
+        }
+    };
+
+    struct Node : public Base {
+        Node *Parent;
+        std::vector<std::pair<int, Node *>> Children;
+
+        Node(std::string ingredient = "", Node *Parent = nullptr) : Base(ingredient) {
+            this->Children = {};
+            this->Parent = Parent;
+            std::cout << "New Node called " << ingredient << " was made!" << std::endl;
+        }
+
+        ~Node() {
+            std::cout << "Node called " << ingredient << " was deleted!" << std::endl;
+            //recursively deallocate all sub-nodes
+            for (auto &child: Children) {
+                delete child.second;
+            }
+        }
+    };
+
+    Node *head(Node *node) {
+        ///@brief This function will return the head node of the tree
+        ///@param node The node to find the head of
+        ///@return The head Node of the ingredient tree
+        if (node->Parent) {
+            return node;
+        } else {
+            return head(node->Parent);
+        }
+    }
+} // Node
+
---
+++ main.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <string>
+
+#include "NodeUtility.h"
+
+NodeUtility::Node *populate(NodeUtility::Node *node);
+
+int main() {
+    //prompt user to type in the name of the item they want to create
+    std::string ingredient; //name of the head ingredient item the user wants to create
+    while (true) {
+        std::cout << "What is the name of the ingredient you want to create: ";
+        std::getline(std::cin, ingredient);
+        //strip leading and trailing whitespace from ingredient
+        ingredient.erase(0, ingredient.find_first_not_of(' '));
+        std::cout << std::endl;
+        if (ingredient.empty()) {
+            std::cout << "Please enter a valid ingredient name" << std::endl;
+        } else {
+            break;
+        }
+    }
+    auto head = populate(new NodeUtility::Node(ingredient));
+    delete head;
+    return 0;
+}
+
+NodeUtility::Node *populate(NodeUtility::Node *node) {
+    ///@brief This function will populate the tree with nodes
+    ///@param node The node to populate
+    ///@return The head Node of the ingredient tree
+    //prompt the user to type in their ingredients
+    //output the ingredient trail
+    //create sub-nodes with each ingredient
+    //recursively call this function on each sub-node
+    //return the head of the ingredient tree
+    return node;
+}
