9830643bb9896ddb98f714c8dc88f762df2ab8e6
AzureVauban <haloringman@outlook.com>
2022-11-18 05:30:57 -0600

    Commit - Modification

--- main.cpp
+++ main.cpp
@@ -7,12 +7,13 @@ NodeUtility::Node *populate(NodeUtility::Node *node);
 
 void trail(NodeUtility::Node *node);
 
-namespace temp {
-    //namespace for functions to refactor
-    std::vector<std::string> IngredientsInput(const std::string headingredient = "Head",
-                                              const std::string parentingredient = "Parent") {
+namespace TempFunctions {
+    //namespace for functions to refactor or utilize later
+    std::vector<std::pair<int, std::string>> IngredientsInput(const std::string headingredient = "Head",
+                                                              const std::string parentingredient = "Parent") {
         //prompt the user to type in their ingredients
-        std::vector<std::string> ingredients;
+        std::vector<std::pair<int, std::string>> ingredients;
+        std::cout << "Type in the ingredients you need to create " << parentingredient << ":" << std::endl;
         while (true) {
             std::string ingredient;
             std::getline(std::cin, ingredient);
@@ -21,7 +22,7 @@ namespace temp {
             //parse through the vector if it is not empty
             if (!ingredients.empty()) {
                 for (auto &i: ingredients) {
-                    if (i == ingredient) {
+                    if (i.second == ingredient) {
                         IsDuplicate = true;
                         break;
                     }
@@ -36,30 +37,65 @@ namespace temp {
             } else if (parentingredient == ingredient) { //if the user input is same as parent ingredient
                 std::cout << "You cannot use the same ingredient as the parent ingredient!" << std::endl;
             } else {
-                ingredients.emplace_back(ingredient);
+                ingredients.emplace_back(std::make_pair(ingredients.size(), ingredient));
             }
         }
         //out the ingredients
         std::cout << "INPUTS:" << std::endl;
         for (const auto &ingredient: ingredients) {
-            std::cout << ingredient << std::endl;
+            std::cout << ingredient.first << " : "
+                      << ingredient.second << std::endl;
         }
         //return the vector of strings
         return ingredients;
     }
-    std::vector<std::pair<int,std::string>> MockPopulate(const std::string Head = "Head",
-                                                         const std::string Parent = "Parent",
-                                                         std::vector<std::pair<int,std::string>> GlobalIngredients = {}) {
-    {
-        // emplace back std::make_pair
-        // return the vector
+
+    std::vector<std::pair<int, std::string>> MockPopulate(const std::string Head = "Head",
+                                                          const std::string Parent = "Parent",
+                                                          std::vector<std::pair<int, std::string>> GlobalIngredients = {}) {
+
+        ///@brief This function will mock populate the tree
+        ///@param Head The head ingredient
+        ///@param Parent The parent ingredient
+        ///@param GlobalIngredients The vector of ingredients to populate the tree with
+        ///@return The vector of ingredients to populate the tree with
+        //todo finish and debug this function
+        //set positional index to start at
+        int beginIndex = 0; //the index to start at
+        if (!GlobalIngredients.empty()) {
+            beginIndex = GlobalIngredients.size()-1;
+        }
+        //@Note use emplace back std::make_pair
+        auto ingredients = TempFunctions::IngredientsInput(Head, Parent);
+        // set the positional index to end at
+        int endIndex = ingredients.size()-1+beginIndex;
+        //add the ingredients to the global vector
+        for (auto &ingredient: ingredients) {
+            GlobalIngredients.emplace_back(std::make_pair(GlobalIngredients.size(), ingredient.second));
+        }
+        //recursively call function on the new sttrings in the vector
+        for (int i = beginIndex; i <= endIndex; ++i) {
+            MockPopulate(Head, GlobalIngredients.at(i).second, GlobalIngredients);
+        }
+        // return the modifed vector
         return GlobalIngredients;
     }
-    bool HasDuplicate(std::vector<std::pair<int,std::string>> UserInputs)
-    {
+
+    bool HasDuplicate(
+            const std::vector<std::pair<int, std::string>> UserInputs,
+            const std::string Ingredient) {   ///@brief This function will check if the user inputs have any duplicates
+        ///@param UserInputs The vector of user inputs
+        ///@return True if there are duplicates, false if there are not
+        //todo finish and debug this function
         return false;
     }
 
+    void OutputToFile(std::ostream &output, const std::vector<std::pair<int, std::string>> &UserInputs) {
+        ///@brief This function will output the user inputs to a file
+        ///@param output The output stream to output to
+        ///@param UserInputs The vector of user inputs to output
+        //todo finish and debug this function
+    }
 }
 
 int main() {
@@ -77,7 +113,7 @@ int main() {
             break;
         }
     }
-    temp::MockPopulate();
+    TempFunctions::MockPopulate(ingredient);
     //auto head = populate(new NodeUtility::Node(ingredient));
     //delete head;
     return 0;
