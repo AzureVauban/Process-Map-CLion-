c5937ab84d79d9be2b4ea7e8f0fb45727317799b
AzureVauban <haloringman@outlook.com>
2022-11-18 23:22:55 -0600

    Commit - Modification

---
+++ Include/Nodes.h
@@ -0,0 +1,156 @@
+//
+// Created by halor on 11/17/2022.
+//
+
+#pragma once
+
+#include <iostream>
+#include <utility>
+#include <vector>
+
+namespace Nodes {
+
+    struct Base {
+        std::string ingredient;
+        int amountonhand;
+        int amountneeded;
+        int amountmadepercraft;
+        int amountresulted;
+
+        Base(std::string ingredient = "",
+             int amountonhand = 0,
+             int amountmadepercraft = 0,
+             int amountneeded = 0) {
+            this->ingredient = ingredient;
+            this->amountonhand = amountonhand;
+            this->amountmadepercraft = amountmadepercraft;
+            this->amountneeded = amountneeded;
+            this->amountresulted = 0;
+        }
+    };
+
+    struct Node : public Base {
+        Node *Parent;
+        static int instances;
+        int instancekey = 0;
+        int generation;
+        int population = 0;
+
+        std::vector<std::pair<int, Node *>> Children;
+        std::vector<std::pair<const std::string &, int>> ResultedAmountsBuffer;
+
+        Node(std::string ingredient = "", Node *Parent = nullptr)
+                : Base(ingredient) {
+            this->Children = {};
+            this->Parent = Parent;
+            this->instancekey = instances;
+            if (this->Parent) {
+                this->generation = this->Parent->generation + 1;
+                this->Parent->Children.emplace_back(this->instancekey, this);
+            } else {
+                this->generation = 0;
+            }
+            instances++;
+            this->population = this->CountNodes(this->population);
+            //? std::cout << "New Node called " << ingredient << " was made!" << std::endl;
+        }
+
+        ~Node() {
+            //? std::cout << "Node called " << ingredient << " was deleted!" << std::endl;
+            //recursively deallocate all sub-nodes
+            for (auto &child: Children) {
+                delete child.second;
+            }
+        }
+
+        int CountNodes(int &countpopulation) { //todo test this function
+            ///@brief This function will count how many Node instances are in the ingredient tree
+            ///@param countpopulation The countpopulation of the ingredient tree
+            ///@return The countpopulation of the ingredient tree
+            countpopulation += 1;
+            //recursively call this function on each sub-node
+            for (auto &child: this->Children) {
+                child.second->CountNodes(countpopulation);
+            }
+            return countpopulation;
+        }
+
+        //todo make function to update the CountNodes variable of all Nodes in a tree
+        void UpdatePopulation(const int countpopulation) {
+            ///@brief This function will update the CountNodes variable of all Nodes in a tree
+            ///@param countpopulation The countpopulation of the ingredient tree
+            this->population = countpopulation;
+            //recursively call this function on each sub-node
+            for (auto &child: this->Children) {
+                child.second->UpdatePopulation(countpopulation);
+            }
+        }
+
+        int recursivearithmetic() {
+            if (this->Parent) {
+                this->Parent->recursivearithmetic();
+            }
+            return this->amountresulted;
+        }
+
+        int reversearithmetic(Node *node, const int desiredamount) {
+            for (auto &child: node->Children) {
+                child.second->reversearithmetic(child.second, amountonhand);
+            }
+            return node->amountonhand;
+        }
+    };
+
+    int Node::instances = 0;
+
+    Node *head(Node *node) {
+        ///@brief This function will return the head node of the tree
+        ///@param node The node to find the head of
+        ///@return The head Node of the ingredient tree
+        while (node->Parent) { //traverse to the head of the ingredient tree
+            node = node->Parent;
+        }
+        return node;
+    }
+
+    namespace temporary {
+        //temporary functions for testing output to Console
+        std::vector<std::string> AllIngredients(const Node *node, //* start this function off with head Node
+                                                std::vector<std::string> &Ingredients) {
+            ///@brief This function will return a vector of all ingredients in the tree
+            ///@param node The node add the ingredient to the vector from
+            ///@param Ingredients The vector of ingredients
+            ///@return The vector of ingredients
+            Ingredients.emplace_back(node->ingredient);
+            for (auto &child: node->Children) {
+                AllIngredients(child.second, Ingredients);
+            }
+            return Ingredients;
+        }
+        std::vector<std::string> AlignAllIngredients(Node* StartingNode){
+            std::vector<std::string> Ingredients;
+            Ingredients = AllIngredients(head(StartingNode), Ingredients);
+            //find the length of the longest string
+            int lengthLongeststring = Ingredients[0].length();
+            for (auto &ingredient: Ingredients) {
+                if (ingredient.length() > lengthLongeststring) {
+                    lengthLongeststring = ingredient.length();
+                }
+            }
+            lengthLongeststring += 1;
+            //append whitespace to the end of each string to make them all the same length
+            for (auto &ingredient: Ingredients) {
+                while (ingredient.length() < lengthLongeststring) {
+                    ingredient.append(" ");
+                }
+            }
+            //test output alignment
+            for (int i = 0; i < Ingredients.size(); i++) {
+                std::cout << i << ": " << Ingredients[i] << "| " <<&Ingredients[i] << std::endl;
+            }
+            //return the modified vector
+            return Ingredients;
+        }
+    }
+} // Node
+
--- main.cpp
+++ main.cpp
@@ -2,165 +2,21 @@
 #include <string>
 //#include <iomanip>
 
-#include "Include/NodeUtility.h"
+#include "Include/Nodes.h"
 
-NodeUtility::Node *populate(NodeUtility::Node *node);
+Nodes::Node *populate(Nodes::Node *head);
 
-void trail(NodeUtility::Node *node);
+Nodes::Node *subpopulate(Nodes::Node *Parent, const std::string &ingredient);
 
-namespace TempFunctions {
-    //namespace for functions to refactor or utilize later
-    struct simpleNode{
-        std::string Ingredient;
-        simpleNode* Next;
-        std::vector<simpleNode*> Prev;
-        //constructor
-        simpleNode(std::string Ingredient = "",
-                   simpleNode* Next = nullptr)
-        {
-            this->Ingredient = Ingredient;
-            this->Next = Next;
-            if (this->Next)
-            {
-                this->Next->Prev.emplace_back(this);
-            }
-        }
-
-        //setters
-        void setIngredient(std::string &basicString) {
-            this->Ingredient = basicString;
-        }
-        //destructor
-        ~simpleNode() {
-            for (auto &i : Prev)
-            {
-                delete i;
-            }
-        }
-
-        //getters
-        std::vector<std::string> AllIngredients(std::vector<std::string>& Ingredients){
-            Ingredients.emplace_back(this->Ingredient);
-            for (auto &i : this->Prev)
-            {
-                i->AllIngredients(Ingredients);
-            }
-            return Ingredients;
-        }
-    };
-
-    //namespace Prototypes
-    bool HasDuplicate( //check if the vector has a duplicate string
-            const std::vector<std::pair<int, std::string>> UserInputs,
-            const std::string Ingredient); //todo pass arguments by reference
-    std::vector<std::string> alignoutput(std::vector<std::string> &lines);
-
-    //namespace Definitions
-    std::vector<std::pair<int, std::string>> IngredientsInput(const std::string headingredient = "Head",
-                                                              const std::string parentingredient = "Parent") {
-        //prompt the user to type in their ingredients
-        std::vector<std::pair<int, std::string>> ingredients;
-        std::cout << "Type in the ingredients you need to create " << parentingredient << ":" << std::endl;
-        while (true) {
-            std::string ingredient;
-            std::getline(std::cin, ingredient);
-            //strip leading and trailing whitespace from ingredient
-            bool IsDuplicate = false;
-            //parse through the vector if it is not empty
-            if (TempFunctions::HasDuplicate(ingredients, ingredient)) { //if the user input is repeated
-                std::cout << "You already typed that ingredient in!" << std::endl;
-            } else if (ingredient.empty()) { //if the user input is empty break out of the loop
-                break;
-            } else if (headingredient == ingredient) { //if the user input is the same as the head ingredient
-                std::cout << "You cannot use the same ingredient as the head ingredient!" << std::endl;
-            } else if (parentingredient == ingredient) { //if the user input is same as parent ingredient
-                std::cout << "You cannot use the same ingredient as the parent ingredient!" << std::endl;
-            } else {
-                ingredients.emplace_back(std::make_pair(ingredients.size(), ingredient));
-            }
-        }
-        //out the ingredients
-        std::cout << "INPUTS:" << std::endl;
-        for (const auto &ingredient: ingredients) {
-            std::cout << ingredient.first << " : "
-                      << ingredient.second << std::endl;
-        }
-        //return the vector of strings
-        return ingredients;
-    }
-
-    std::vector<std::pair<int, std::string>> MockPopulate(const std::string &Head,
-                                                          const std::string &Parent,
-                                                          std::vector<std::pair<int, std::string>> &GlobalIngredients) {
-
-        ///@brief This function will mock populate the tree
-        ///@param Head The head ingredient
-        ///@param Parent The parent ingredient
-        ///@param GlobalIngredients The vector of ingredients to populate the tree with
-        ///@return The vector of ingredients to populate the tree with
-        //todo finish and debug this function
-        //set positional index to start at
-        int beginIndex = 0; //the index to start at
-        if (!GlobalIngredients.empty()) {
-            beginIndex = GlobalIngredients.size() - 1;
-        }
-        //@Note use emplace back std::make_pair
-        auto ingredients = TempFunctions::IngredientsInput(Head, Parent);
-        // set the positional index to end at
-        int endIndex = ingredients.size() - 1 + beginIndex;
-        //add the ingredients to the global vector
-        for (auto &ingredient: ingredients) {
-            GlobalIngredients.emplace_back(std::make_pair(GlobalIngredients.size(), ingredient.second));
-        }
-        //recursively call function on the new sttrings in the vector
-        for (int i = beginIndex; i <= endIndex; ++i) {
-            MockPopulate(Head, GlobalIngredients.at(i).second, GlobalIngredients);
-        }
-        // return the modifed vector
-        return GlobalIngredients;
-    }
-
-    bool HasDuplicate(
-            const std::vector<std::pair<int, std::string>> UserInputs,
-            const std::string Ingredient) {   ///@brief This function will check if the user inputs have any duplicates
-        //todo finish and debug this function
-        ///@param UserInputs The vector of user inputs
-        ///@return True if there are duplicates, false if there are not
-        for (const auto &input: UserInputs) {
-            if (input.second == Ingredient) {
-                return true;
-            }
-        }
-        return false;
-    }
-
-    void OutputToFile(std::ostream &output, const std::vector<std::pair<int, std::string>> &UserInputs) {
-        ///@brief This function will output the user inputs to a file
-        ///@param output The output stream to output to
-        ///@param UserInputs The vector of user inputs to output
-        //todo finish and debug this function
-    }
-
-    std::vector<std::string> alignoutput(std::vector<std::string> &lines) {
-        ///@brief This function will align the output
-        ///@param lines The vector of strings to align
-        ///@return The vector of strings aligned
-        ///@Note utilize the std::setw() function and iomanip library
-        //figure out what is the longest string in the vector
-        //append whitespace to the end of the strings to align output
-        //return the vector of strings for output
-        //todo finish and debug this function
-        return lines;
-    }
-}
+void trail(Nodes::Node *node);
 
 int main() {
-    //prompt user to type in the name of the item they want to create
-    std::string ingredient; //name of the head ingredient item the user wants to create
+    // prompt user to type in the name of the item they want to create
+    std::string ingredient; // name of the head ingredient item the user wants to create
     while (true) {
         std::cout << "What is the name of the ingredient you want to create?" << std::endl;
         std::getline(std::cin, ingredient);
-        //strip leading and trailing whitespace from ingredient
+        // strip leading and trailing whitespace from ingredient
         ingredient.erase(0, ingredient.find_first_not_of(' '));
         std::cout << std::endl;
         if (ingredient.empty()) {
@@ -169,63 +25,57 @@ int main() {
             break;
         }
     }
-    std::vector<std::pair<int, std::string>> myInputs = {};
-    myInputs = TempFunctions::MockPopulate(ingredient, "", myInputs);
-    for (const auto &input: myInputs) { //output all the inputs to the console
-        std::cout << input.first << " : " << input.second << std::endl;
-    }
-    //auto head = populate(new NodeUtility::Node(ingredient));
-    //delete head;
+    auto head = populate(new Nodes::Node(ingredient));
+    Nodes::temporary::AlignAllIngredients(head);
+    std::cout << "Populate of Tree: " << head->population << std::endl;
+    delete head;
     return 0;
 }
 
-NodeUtility::Node *populate(NodeUtility::Node *node) {
+Nodes::Node *populate(Nodes::Node *head) {
     ///@brief This function will populate the tree with nodes
-    ///@param node The node to populate
+    ///@param node The head to populate
     ///@return The head Node of the ingredient tree
-    std::cout << "Type in the ingredients you need to create " << node->ingredient << ":" << std::endl;
-    //output the ingredient trail
-    if (node->Parent) {
-        //trail(node);
+    std::cout << "Type in the ingredients you need to create " << head->ingredient << ":" << std::endl;
+    // output the ingredient trail
+    if (head->Parent) {
+        std::cout << "TRAIL: ";
+        trail(head);
     }
-    //prompt the user to type in their ingredients
+    // prompt the user to type in their ingredients
     std::vector<std::string> ingredients;
     while (true) {
         std::string ingredient;
         std::getline(std::cin, ingredient);
-        //strip leading and trailing whitespace from ingredient
-        bool IsDuplicate = false;
-        //parse through the vector if it is not empty
-        if (!ingredients.empty()) {
-            for (auto &i: ingredients) {
-                if (i == ingredient) {
-                    IsDuplicate = true;
-                    break;
-                }
-            }
-        }
-        if (IsDuplicate) { //if the user input is repeated
-            std::cout << "You already typed that ingredient in!" << std::endl;
-        } else if (ingredient.empty()) { //if the user input is empty break out of the loop
+        if (ingredient.empty()) {
             break;
-        } else if (head(node)->ingredient == ingredient) { //if the user input is the same as the head ingredient
-            std::cout << "You cannot use the same ingredient as the head ingredient!" << std::endl;
-        } else if (node->Parent->ingredient == ingredient) { //if the user input is same as parent ingredient
-            std::cout << "You cannot use the same ingredient as the parent ingredient!" << std::endl;
         } else {
             ingredients.emplace_back(ingredient);
         }
     }
-//create sub-nodes with each ingredient
-//recursively call this function on each sub-node
-//return the head of the ingredient tree
-    if (node->Parent) {
-        NodeUtility::head(node);
+    // create sub-nodes with each ingredient
+    for (auto &ingredient: ingredients) {
+        subpopulate(head, ingredient);
+        //update the population of the ingredient tree
+        int p = 0;
+        Nodes::head(head)->UpdatePopulation(Nodes::head(head)->CountNodes(p));
     }
-    return node;
+    // recursively call this function on each sub-head
+    for (auto &subnode: head->Children) {
+        populate(subnode.second);
+    }
+    // return the head of the ingredient tree
+    if (head->Parent) {
+        Nodes::head(head);
+    }
+    return head;
 }
 
-void trail(NodeUtility::Node *node) {
+Nodes::Node *subpopulate(Nodes::Node *Parent,const std::string &Ingredient) {
+    return new Nodes::Node(Ingredient, Parent);
+}
+
+void trail(Nodes::Node *node) {
     ///@brief This function will output the ingredient trail
     ///@param node The node to output the ingredient trail of
     while (true) {
